d43de09222f97fddfa0d8b812995e5d698f1f8dd
Igor Davydenko <playpauseandstop@gmail.com>
2009-04-03 22:50:37 +0300

    Try to load settings from current directory, then from current basename subdirectory, than from all current directory subdirectories.

--- django-manage.py
+++ django-manage.py
@@ -1,34 +1,65 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
+
 
 __author__ = 'Igor Davydenko <playpauseandstop@gmail.com>'
 __license__ = 'GNU General Public License v.3 or above'
-__version__ = '0.1-beta'
+__version__ = '0.1'
 
 if __name__ == '__main__':
-    try:
-        dirname = os.environ['PWD']
-    except KeyError:
-        import subprocess
-        cmd = subprocess.Popen('cd', shell=True, stdout=subprocess.PIPE)
-        dirname = cmd.communicate()[0].strip()
+    dirname = os.getcwd()
     sys.path.append(dirname)
+
+    childs = os.listdir(dirname)
+    childs.sort()
+
+    subdirs = []
+
+    for name in childs:
+        if name[0] == '.':
+            continue
+
+        if os.path.isdir(os.path.join(dirname, name)):
+            subdirs.append(name)
 
     try:
         settings = 'settings'
+
         try:
             __import__(settings)
-        except ImportError, e:
+        except ImportError:
             settings = os.path.basename(dirname) + '.settings'
-            __import__(settings)
-    except ImportError, e:
+
+            try:
+                __import__(settings)
+            except ImportError:
+                imported = False
+
+                for subdir in subdirs:
+                    settings = subdir + '.settings'
+
+                    try:
+                        __import__(settings)
+                    except ImportError:
+                        pass
+                    else:
+                        imported = True
+                        break
+
+                if not imported:
+                    raise ImportError
+    except ImportError:
         sys.stderr.write(
-            "Error: Can't find the file 'settings.py' in the current work " +
-            "directory %r. It appears you've customized things.\n" % dirname +
-            "You'll have to run django-admin.py, passing it your settings " +
-            "module.\n(If the file settings.py does indeed exist, it's " +
-            "causing an ImportError somehow.)\n"
+            "Error: Can't find the file 'settings.py' in the current work " \
+            "directory %r and all subdirectories %r. It appears you've " \
+            "customized things.\nYou'll have to run django-manage.py, " \
+            "passing it your settings module.\n(If the file settings.py does " \
+            "indeed exist, it's causing an ImportError somehow.)\n" % (
+                dirname,
+                subdirs,
+            )
         )
         sys.exit(1)
 
@@ -38,7 +69,7 @@ if __name__ == '__main__':
         from django.core.management import ManagementUtility
     except ImportError, e:
         sys.stderr.write(
-            "Error: Can't load 'django' library on current environment. " +
+            "Error: Can't load `django` library on current environment. " \
             "Please check your python system path and try again later.\n"
         )
         sys.exit(1)
